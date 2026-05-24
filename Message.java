52ac2bf2db375ec5b0c39dab4ed8073a66fd58ef
ST10518408 <ST10518407@rcconnect.edu.za>
2026-05-24 23:54:27 +0200

    Update Message.java

--- Message.java
+++ Message.java
@@ -13,6 +13,7 @@ import java.io.IOException;
 public class Message {
 
     //LOGIN CLASS//
+    //part1 login fields//
     static class Login {
         private String firstName;
         private String lastName;
